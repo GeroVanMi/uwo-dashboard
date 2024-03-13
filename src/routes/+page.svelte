b002b6f0bf407c6725b13e9071088c4752d9ae61
Gerome Meyer <gerome.meyer@protonmail.com>
2024-03-13 18:04:02 +0100

    Added dropdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,29 @@
   import * as d3 from "d3";
 
   let data = d3.ticks(-2, 2, 200).map(Math.sin);
+
+  // TODO: We want to fetch this from a CSV file.
+  let sources = {
+    1: "bf_plsZUL1100_inflow_ara",
+    42: "bf_f03_11e_russikerstr",
+    48: "bf_f07_23_bahnhofstr",
+    71: "bf_f02_555_mesikerst",
+  };
+
+  $: selectedSource = 1;
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-10 text-center flex flex-col items-center">
     <h2 class="h2">UWO Dashboard</h2>
+
+    <select class="select" bind:value={selectedSource}>
+      {#each Object.entries(sources) as [index, source]}
+        <option value={index}>{source}</option>
+      {/each}
+    </select>
+    {selectedSource}
+
     <LinePlot {data} />
   </div>
 </div>
